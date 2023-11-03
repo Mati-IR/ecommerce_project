1294928db75428778d7f7fbb2f6845450ac9bc2e
Mati-IR <mpabich4@gmail.com>
2023-11-03 16:51:23 +0100

    Implemented open-source authentication handler

--- backend/identity_microservice/MVC_user_identity.py
+++ backend/identity_microservice/MVC_user_identity.py
@@ -1,69 +1,125 @@
-from fastapi import FastAPI, HTTPException
-from sqlalchemy import create_engine, Column, Integer, String, MetaData
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
-from databases import Database
-from schemes import *
-import logging
-
-DATABASE_URL = "mysql+mysqlconnector://user:password@db:3306/SayHelloToMyLittleFriend"
-
-database = Database(DATABASE_URL)
-metadata = MetaData()
-Base = declarative_base(metadata=metadata)
-
-class User(Base):
-    __tablename__ = "users"
-
-    id = Column(Integer, primary_key=True, index=True)
-    name = Column(String(255))
-    email = Column(String(255), index=True, unique=True)
-
-engine = create_engine(DATABASE_URL)
-SessionLocal = sessionmaker(bind=engine)
+from fastapi import FastAPI, HTTPException, Security
+from fastapi.middleware.cors import CORSMiddleware
+from database.query import query_get, query_put, query_update
+from fastapi.security import HTTPAuthorizationCredentials, HTTPBearer
+from fastapi.responses import JSONResponse
+from fastapi.encoders import jsonable_encoder
+from user import Auth, SignInRequestModel, SignUpRequestModel, UserAuthResponseModel, UserUpdateRequestModel, UserResponseModel, register_user, signin_user, update_user, get_all_users, get_user_by_id
 
 app = FastAPI()
 
-@app.on_event("startup")
-async def startup():
-    try:
-        await database.connect()
-        Base.metadata.create_all(engine)
-    except Exception as e:
-        logging.error(f"Database connection error: {str(e)}")  # Log the error
-        # You can take additional actions here, like raising a specific exception or sending an alert.
+origins = [
+    "http://localhost",
+    "http://localhost:8000",
+    "http://localhost:3000",
+    "http://localhost:3001",
+    "http://localhost:4000",
+    "http://localhost:19006"
+]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+security = HTTPBearer()
+auth_handler = Auth()
 
 
-@app.on_event("shutdown")
-async def shutdown():
-    await database.disconnect()
+###############################
+########## Auth APIs ##########
+###############################
 
-@app.post("/users/")
-async def create_user(user: UserCreate):  # Use the Pydantic model for input validation
-    db_user = User(name=user.name, email=user.email)  # Create a SQLAlchemy User instance
+@app.post('/v1/signup', response_model=UserAuthResponseModel)
+def signup_api(user_details: SignUpRequestModel):
+    """
+    This sign-up API allow you to register your account, and return access token.
+    """
+    user = register_user(user_details)
+    access_token = auth_handler.encode_token(user_details.email)
+    refresh_token = auth_handler.encode_refresh_token(user_details.email)
+    return JSONResponse(status_code=200, content={'token': {'access_token': access_token, 'refresh_token': refresh_token}, 'user': user})
 
-    # Insert the user into the database
-    try:
-        db_session = SessionLocal()
-        db_session.add(db_user)
-        db_session.commit()
-        db_session.refresh(db_user)
-    except Exception as e:
-        raise HTTPException(status_code=500, detail="Error creating user in the database")
 
-    return user
+@app.post('/v1/signin', response_model=UserAuthResponseModel)
+def signin_api(user_details: SignInRequestModel):
+    """
+    This sign-in API allow you to obtain your access token.
+    """
+    user = signin_user(user_details.email, user_details.password)
+    access_token = auth_handler.encode_token(user['email'])
+    refresh_token = auth_handler.encode_refresh_token(user['email'])
+    return JSONResponse(status_code=200, content={'token': {'access_token': access_token, 'refresh_token': refresh_token}, 'user': user})
 
-@app.get("/users/{user_id}/{password_hash}")
-async def get_user(user_id: int, password_hash: str):
-    if not database.is_connected:
-        raise HTTPException(status_code=500, detail="Database connection error.")
 
-    query = User.__table__.select().where(User.id == user_id)
-    user = await database.fetch_one(query)
+@app.get('/v1/refresh-token')
+def refresh_token_api(refresh_token: str):
+    """
+    This refresh-token API allow you to obtain new access token.
+    """
+    new_token = auth_handler.refresh_token(refresh_token)
+    return {'access_token': new_token}
 
-    if not user:
-        raise HTTPException(status_code=404, detail="User not found.")
-    elif user.password_hash != password_hash:
-        raise HTTPException(status_code=401, detail="Incorrect password.")
 
-    return user
+################################
+########## Users APIs ##########
+################################
+
+@app.get("/v1/users", response_model=list[UserResponseModel])
+def get_all_users_api(credentials: HTTPAuthorizationCredentials = Security(security)):
+    """
+    This users get API allow you to fetch all user data.
+    """
+    token = credentials.credentials
+    if (auth_handler.decode_token(token)):
+        user = get_all_users()
+        return JSONResponse(status_code=200, content=jsonable_encoder(user))
+    return JSONResponse(status_code=401, content={'error': 'Faild to authorize'})
+
+
+@app.get("/v1/user/{user_id}", response_model=UserResponseModel)
+def get_user_api(user_id: int, credentials: HTTPAuthorizationCredentials = Security(security)):
+    """
+    This user API allow you to fetch specific user data.
+    """
+    token = credentials.credentials
+    if (auth_handler.decode_token(token)):
+        user = get_user_by_id(user_id)
+        return JSONResponse(status_code=200, content=jsonable_encoder(user))
+    return JSONResponse(status_code=401, content={'error': 'Faild to authorize'})
+
+
+@app.post("/v1/user/update", response_model=UserResponseModel)
+def update_user_api(user_details: UserUpdateRequestModel, credentials: HTTPAuthorizationCredentials = Security(security)):
+    """
+    This user update API allow you to update user data.
+    """
+    token = credentials.credentials
+    if (auth_handler.decode_token(token)):
+        user = update_user(user_details)
+        return JSONResponse(status_code=200, content=jsonable_encoder(user))
+    return JSONResponse(status_code=401, content={'error': 'Faild to authorize'})
+
+
+###############################
+########## Test APIs ##########
+###############################
+
+@app.get('/secret')
+def secret_data_api(credentials: HTTPAuthorizationCredentials = Security(security)):
+    """
+    This secret API is just for testing. Need access token to access this API.
+    """
+    token = credentials.credentials
+    if (auth_handler.decode_token(token)):
+        return 'Top Secret data only authorized users can access this info'
+
+
+@app.get('/not-secret')
+def not_secret_data_api():
+    """
+    This not-secret API is just for testing.
+    """
+    return 'Not secret data'
