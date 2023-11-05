354249f878c585cbd1a497d837bf133723ac2e4b
Mati-IR <mpabich4@gmail.com>
2023-11-05 11:47:34 +0100

    Added categories to listings_db

--- database/listings_db/db/init.sql
+++ database/listings_db/db/init.sql
@@ -18,3 +18,24 @@ CREATE TABLE IF NOT EXISTS `listing_photos` (
   `photo_url` VARCHAR(255) NOT NULL,
   FOREIGN KEY (`listing_id`) REFERENCES `listings`(`id`) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- Categories Table
+CREATE TABLE IF NOT EXISTS `categories` (
+  `id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  `name` VARCHAR(255) NOT NULL UNIQUE,
+  `description` TEXT
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- Insert extended categories
+INSERT INTO `categories` (`name`, `description`) VALUES
+('For Sale', 'Items for sale by owner'),
+('Jobs', 'Job listings and employment opportunities'),
+('Housing', 'Real estate, rentals, and shared accommodations'),
+('Services', 'Local services offered by professionals and businesses'),
+('Vehicles', 'Cars, trucks, motorcycles, and other vehicles'),
+('Electronics', 'Computers, mobile phones, gadgets, and other electronics'),
+('Furniture', 'Home and office furniture'),
+('Clothing & Accessories', 'Apparel, shoes, and fashion accessories'),
+('Free Stuff', 'Items being given away for free'),
+('Pets', 'Pets for sale, adoption, and pet services'),
+('Gigs', 'Short-term jobs and freelance opportunities');
