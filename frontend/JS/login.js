e11cf0a02fbd0f73d30ecc7180b71deee70ae706
Mati-IR <mpabich4@gmail.com>
2023-11-08 18:24:53 +0100

    Adapted login form, it now handles login fail and success

--- frontend/JS/login.js
+++ frontend/JS/login.js
@@ -17,3 +17,50 @@
 			}, false)
 		})
 })()
+
+document.addEventListener('DOMContentLoaded', function() {
+
+	// Handle the login form submission
+	document.getElementById('loginForm').addEventListener('submit', function(event) {
+		event.preventDefault(); // Prevent the form from submitting the traditional way
+
+		// Get the form data
+		const loginData = {
+			email: document.getElementById('email').value,
+			password: document.getElementById('password').value
+		};
+
+		// Send the loginData to the API gateway for authentication
+		fetch('http://127.0.0.1:8000/login', {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json'
+			},
+			body: JSON.stringify(loginData)
+		})
+			.then(response => {
+				if (!response.ok) {
+					throw new Error(`Network response was not ok: ${response.statusText}`);
+				}
+				return response.json();
+			})
+			.then(data => {
+				// Handle successful login
+				// For demonstration purposes, we'll just show an alert
+				alert('Login successful!');
+
+				// TODO: Handle any additional tasks after successful login, e.g., redirecting the user or showing a welcome message
+			})
+			.catch(error => {
+				// Handle login errors
+				// For demonstration purposes, we'll just show an alert
+				alert('Login failed: ' + error.message);
+
+				// TODO: Handle any additional tasks after failed login, e.g., showing a specific error message to the user
+			});
+
+		// Clear the form fields after submission
+		//event.target.reset();
+	});
+
+});
