d4ac91e292cd7a967652f8cefecc089d0794a380
MrGrzesiek <78262537+MrGrzesiek@users.noreply.github.com>
2024-01-09 13:00:02 +0100

    Full listing view fixes

--- frontend/JS/ListingViewConstructor.js
+++ frontend/JS/ListingViewConstructor.js
@@ -39,10 +39,23 @@ function generateFullPreview() {
             sellerId = data[0].creator_id;
             // Populate the data in the HTML elements
             document.getElementById('title').textContent = data[0].title;
-            document.getElementById('creation_date').textContent = data[0].creationDate;
-            document.getElementById('category').textContent = data[0].category;
+            document.getElementById('creation_date').innerHTML = '<strong>Data dodania: </strong> ' + konwertujDate(data[0].creation_date);
+
+            //wyszukiwanie nazwy kategori na podstawie id i jej tlumaczenie
+            getCategories()
+            .then(categories => {
+              const categoryName = findCategoryById(categories, data[0].category_id, translations);
+              document.getElementById('category').textContent = categoryName;
+            })
+            .catch(error => {
+              // Tutaj możesz obsłużyć błąd, jeśli wystąpi
+              console.error('Error:', error);
+              document.getElementById('category').textContent = 'Brak';
+            });
+
             document.getElementById('description').textContent = data[0].description;
-            document.getElementById('price').textContent = data[0].price + "zł";
+            document.getElementById('price').textContent = data[0].price + " zł";
+            
             document.getElementById('email').textContent = data[0].email;
             document.getElementById('phone').textContent = data[0].phone;
             console.log('Location:', data[0].location);
@@ -70,7 +83,9 @@ function generateFullPreview() {
                 .then(data => {
                     // Populate the data in the HTML elements
                     console.log('Seller:', data)
-                    document.getElementById('seller').textContent = data[0].name;
+                    document.getElementById('seller').innerHTML = '<strong>Wystawiający: </strong> ' + data[0].name;
+                    document.getElementById('email').innerHTML = '<strong>Email: </strong> ' + data[0].email;
+                    document.getElementById('phone').innerHTML = '<strong>Telefon: </strong> ' + data[0].phone;
                 })
                 .catch(error => {
                     console.error('Error fetching seller data:', error);
@@ -124,40 +139,43 @@ function generateFullPreview() {
         .catch(error => {
             console.error('Error fetching listing data:', error);
         });
-
-
-
-    // fetch images
-    /*for(let i = 0; i < amountOfImages; i++) {
-        fetch(ApiGateway + '/images/' + listingId + '/' + i)
-            .then(response => response.json())
-            .then(data => {
-                // Populate the data in the HTML elements
-                console.log('Image:', data);
-                document.getElementById('image' + i).src = data;
-            })
-            .catch(error => {
-                console.error('Error fetching images:', error);
-            });
-    }*/
-
 }
 
 generateFullPreview();
 
 function createGallery(images) {
-    const carouselContainer = document.querySelector('.carousel-container');
-    const image = document.querySelector('.carousel-image');
-    const prevBtn = document.getElementById('prevBtn');
-    const nextBtn = document.getElementById('nextBtn');
-    const carouselIndicators = document.querySelector('.carousel-indicators-prime');
-  
-    const fullscreenContainer = document.querySelector('.fullscreen-container');
-    const fullscreenImage = document.querySelector('.fullscreen-image');
-    const fullscreenPrevBtn = document.getElementById('fullscreenPrevBtn');
-    const fullscreenNextBtn = document.getElementById('fullscreenNextBtn');
-    const fullscreenIndicators = document.querySelector('.fullscreen-indicators');
-    const fullscreenClose = document.querySelector('.fullscreen-close');
+  const creationDateElement = document.getElementById('creation_date');
+
+  // Create a div for the new content
+  const newContent = document.createElement('div');
+  newContent.innerHTML = `
+    <div class="carousel-container">
+      <div class="carousel-slide">
+        <img alt="Carousel Slide" class="carousel-image">
+          <div class="carousel-navigation">
+            <button id="prevBtn">&lt;</button>
+            <button id="nextBtn">&gt;</button>
+          </div>
+      </div>
+    </div>
+    <div class="carousel-indicators-prime"></div>`;
+
+  // Insert the new content after the element with id 'creation_date'
+  creationDateElement.appendChild(newContent);
+
+
+  const carouselContainer = document.querySelector('.carousel-container');
+  const image = document.querySelector('.carousel-image');
+  const prevBtn = document.getElementById('prevBtn');
+  const nextBtn = document.getElementById('nextBtn');
+  const carouselIndicators = document.querySelector('.carousel-indicators-prime');
+
+  const fullscreenContainer = document.querySelector('.fullscreen-container');
+  const fullscreenImage = document.querySelector('.fullscreen-image');
+  const fullscreenPrevBtn = document.getElementById('fullscreenPrevBtn');
+  const fullscreenNextBtn = document.getElementById('fullscreenNextBtn');
+  const fullscreenIndicators = document.querySelector('.fullscreen-indicators');
+  const fullscreenClose = document.querySelector('.fullscreen-close');
   
     let currentSlide = 0;
     let fullscreenCurrentSlide = 0;
@@ -249,3 +267,55 @@ function createGallery(images) {
     changeSlide();
   }
   
+async function getCategories() {
+    try {
+        const response = await fetch(ApiGateway + '/categories');
+        const categories = await response.json();
+        return categories;
+    } catch (error) {
+        console.error('Error fetching seller data:', error);
+        return null; // Możesz zwrócić pustą tablicę lub inny typ wartości w przypadku błędu
+    }
+}
+
+const translations  = [
+  'Praca',
+  'Nieruchomości',
+  'Usługi',
+  'Motoryzacja',
+  'Elektronika',
+  'Meble',
+  'Ubrania i Akcesoria',
+  'Za darmo',
+  'Zwierzęta',
+  'Inne'
+];
+
+function findCategoryById(jsonData, categoryId, translationsArray) {
+  try {
+      const categoryName = jsonData[categoryId - 1].name;
+      
+      if (translationsArray && translationsArray[categoryId - 1]) {
+          const translatedName = translationsArray[categoryId - 1];
+          return translatedName !== '' ? translatedName : categoryName;
+      }
+
+      return categoryName;
+  } catch (error) {
+      console.error('Error parsing JSON data:', error);
+      return null; // Możesz zwrócić wartość null lub inny typ wartości w przypadku błędu
+  }
+}
+
+function konwertujDate(data) {
+  const date = new Date(data);
+    
+  const dzien = String(date.getDate()).padStart(2, '0');
+  const miesiac = String(date.getMonth() + 1).padStart(2, '0'); // Dodajemy 1, ponieważ miesiące są indeksowane od zera
+  const rok = date.getFullYear();
+  const godzina = String(date.getHours()).padStart(2, '0');
+  const minuta = String(date.getMinutes()).padStart(2, '0');
+
+  const sformatowanaData = `${dzien}-${miesiac}-${rok} ${godzina}:${minuta}`;
+  return sformatowanaData;
+}
