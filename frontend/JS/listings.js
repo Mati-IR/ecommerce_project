dc5101b24ed1c7bfc728d16763f8828d7937950f
MrGrzesiek <78262537+MrGrzesiek@users.noreply.github.com>
2024-01-10 15:45:56 +0100

    listing create update

--- frontend/JS/listings.js
+++ frontend/JS/listings.js
@@ -1,18 +1,37 @@
+const translatedCategories = [
+    "Prace",
+    "Mieszkania",
+    "Usługi",
+    "Pojazdy",
+    "Elektronika",
+    "Meble",
+    "Odzież i Akcesoria",
+    "Darmowe rzeczy",
+    "Zwierzęta",
+    "Zlecenia",
+    "Inne"
+];
 let categories = [];
 fetch("http://127.0.0.1:8000/categories")
     .then(response => response.json())
     .then(data => {
-        categories = data; // Assuming data is the array of categories
+        const originalCategories = data; // Assuming data is the array of original categories
+        // Translate categories based on translatedCategories array
+        const translatedCategoryOptions = originalCategories.map((category, index) => {
+            return translatedCategories[index] || category; // Use translated category name if available, else use the original name
+        });
+        categories = translatedCategoryOptions; 
+        generateCategoryOptions(translatedCategoryOptions);
     });
 
-function getCategoryID(categoryID) {
-    const category = categories.find(cat => cat.id === categoryID);
-    if (!category) {
-        console.error("Category not found: " + categoryID);
-        console.log("Available categories: " + categories.map(cat => cat.name).join(", "));
-    }
-    return category ? category.id : null;
-}
+    function getCategoryID(categoryName, categories) {
+        for (let i = 0; i < categories.length; i++) {
+          if (categories[i] === categoryName) {
+            return i+1;
+          }
+        }
+        return -1; // Zwraca -1, gdy nie znaleziono nazwy w tablicy
+      }
 
 function getListingByID(listingID) {
     fetch("http://127.0.0.1:8000/" + listingID)
@@ -48,63 +67,116 @@ async function uploadImages(images, listing_id) {
     }
 }
 
-function submitForm() {
+async function submitForm() {
     event.preventDefault();
-
-    event.preventDefault(); // prevent the form from submitting the traditional way
-    const storedUserData = JSON.parse(localStorage.getItem("user"));
-    let userId  = storedUserData.id;
-
-    // get the form data
+    const storedUserData = JSON.parse(localStorage.getItem('user'));
+    const userId = storedUserData.id;
+    const selectedCategoryName = document.getElementById('category').value;
+    const selectedCategoryID = getCategoryID(selectedCategoryName, categories);
+    console.log(selectedCategoryID);
     const newListingData = {
-        /* Get creator_id from local storage */
         creator_id: userId,
-        title: document.getElementById("title").value,
-        description: document.getElementById("description").value,
-        price: parseFloat(document.getElementById("price").value),
-        location: document.getElementById("address").value,
-        category_id: getCategoryID(categories.find(cat => cat.name === document.getElementById("category").value).id)
+        title: document.getElementById('title').value,
+        description: document.getElementById('description').value,
+        price: parseFloat(document.getElementById('price').value),
+        location: document.getElementById('address').value,
+        category_id: selectedCategoryID
     };
-    // if null in any of the fields, alert user and end this event listener
-    if (Object.values(newListingData).includes(null)) {
-        alert("Please fill in all fields!");
-        console.error("Null value in fields: " + Object.keys(newListingData).find(key => newListingData[key] === null));
+
+    if (!validateFormData(newListingData)) {
         return;
     }
 
-    // send the newListingData to the API gateway
-    fetch("http://127.0.0.1:8000/create_listing", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify(newListingData)
-    })
-        .then(response => response.json())
-        .then(data => {
-            console.log('Success:', data);
-            /* return {
-                "status": "success",
-                "message": "Listing created successfully!",
-                "listing": listing_id
-            } */
-            const listingId = data.listing;
-            console.log("listingId = " + listingId);
-
-            let listOfImages = [];
-            const imagesInput = document.getElementById('images');
-            for (let i = 0; i < imagesInput.files.length; i++) {
-                listOfImages.push(imagesInput.files[i]);
-            }
-
-            //console.log(response);
-
-            uploadImages(listOfImages, listingId);
-        })
-        .catch((error) => {
-            console.error('Error:', error);
-            alert("Error creating listing!\n " + error);
+    try {
+        const createListingResponse = await fetch("http://127.0.0.1:8000/create_listing", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(newListingData)
         });
 
+        const listingData = await createListingResponse.json();
+        console.log('Success:', listingData);
+        const listingId = listingData.listing;
+        console.log("listingId = " + listingId);
 
+        let listOfImages = [];
+        const imagesInput = document.getElementById('images');
+        for (let i = 0; i < imagesInput.files.length; i++) {
+            listOfImages.push(imagesInput.files[i]);
+        }
+        if(listOfImages.length>0){
+            await uploadImages(listOfImages, listingId);
+        }
+        
+        clearForm();
+        displaySuccessMessage(); // Show success message
+    } catch (error) {
+        console.error('Error:', error);
+        alert("Error creating listing!\n " + error);
+    }
+}
+
+function displayErrorMessage(message) {
+    const errorMessage = document.createElement('p');
+    errorMessage.classList.add('error-message');
+    errorMessage.textContent = message;
+
+    const errorMessagesContainer = document.getElementById('error-messages-container');
+    errorMessagesContainer.appendChild(errorMessage);
+}
+
+function clearErrorMessages() {
+    const errorMessagesContainer = document.getElementById('error-messages-container');
+    errorMessagesContainer.innerHTML = ''; // Clear existing error messages
+}
+
+function validateFormData(newListingData) {
+    clearErrorMessages();
+
+    for (const [key, value] of Object.entries(newListingData)) {
+        if (typeof value === 'string' && !value.trim()) {
+            displayErrorMessage(`Aby dodać ogłoszenie, uzupełnij wszystkie pola.`);
+            return false;
+        } else if (value === null || value === undefined) {
+            displayErrorMessage(`Aby dodać ogłoszenie, uzupełnij wszystkie pola.`);
+            return false;
+        } else if (key === 'price' && isNaN(value)) {
+            displayErrorMessage(`Podana cena nie jest liczbą.`);
+            return false;
+        }
+    }
+
+    return true;
+}
+
+function displaySuccessMessage() {
+    const successMessage = document.getElementById('success-message');
+    successMessage.style.display = 'block';
+
+    const closeSuccessMessageBtn = document.getElementById('close-success-message');
+    closeSuccessMessageBtn.addEventListener('click', function() {
+        successMessage.style.display = 'none';
+    });
+}
+
+function generateCategoryOptions(categories) {
+    const categorySelect = document.getElementById("category");
+
+    categories.forEach(category => {
+        const option = document.createElement("option");
+        option.value = category;
+        option.textContent = category;
+        categorySelect.appendChild(option);
+    });
+}
+
+function clearForm() {
+    document.getElementById('title').value = '';
+    document.getElementById('description').value = '';
+    document.getElementById('price').value = '';
+    document.getElementById('address').value = '';
+    document.getElementById('category').selectedIndex = 0; // Powrót do pierwszej opcji w polu wyboru kategorii
+    document.getElementById('images').value = '';
 }
